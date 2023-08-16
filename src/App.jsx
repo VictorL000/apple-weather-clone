144723732b1db18ae02a7d7b7f854526268fa1f8
VictorL000 <victorrliu@gmail.com>
2023-08-16 14:27:15 -0400

    added functionality. scroll bar has issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,119 +1,477 @@
 import { useState } from "react";
 import "./App.css";
+import PropTypes from "prop-types";
 
+const FORECASTDAYS = 7;
+const CITY = "toronto-ontario-canada";
+const API_KEY = "398c0c5661ba432481b11820231008";
+let fetched = 0;
+let fetchLimit = 20;
+const SEARCHTIMEOUTMS = 300;
 
 function App() {
+  let [unit, setUnit] = useState("c");
+  let [searchTerm, setSearchTerm] = useState(CITY);
   return (
     <>
-      <Navbar></Navbar>
-      <Hero />
-      <HourlyForecast></HourlyForecast>
-      <DailyForecast></DailyForecast>
+      <Navbar setSearchTerm={setSearchTerm} unit={unit} setUnit={setUnit}></Navbar>
+      <Hero searchTerm={searchTerm} unit={unit} />
+      <HourlyForecast searchTerm={searchTerm} unit={unit}></HourlyForecast>
+      <DailyForecast searchTerm={searchTerm} unit={unit}></DailyForecast>
     </>
   );
 }
 
-function Navbar() {
+function Navbar({ unit, setUnit, setSearchTerm }) {
   return (
     <>
       <div className="navbarBG"></div>
       <div className="navbar">
         <div className="logo">Weather</div>
-        <div className="searchBar"></div>
-        <div className="toggleCF">
-          <input type="checkbox" id="switch" />
-          <label htmlFor="switch">Toggle Celsius Fahrenheit</label>
-        </div>
+        <SearchBar setSearchTerm={setSearchTerm}></SearchBar>
+        <CFButton unit={unit} setUnit={setUnit}></CFButton>
       </div>
     </>
   );
 }
 
-function Hero() {
-  const API_KEY = "398c0c5661ba432481b11820231008";
-  let city = 'Toronto';
-  const [currentTemp, setCurrentTemp] = useState();
-  const [condition, setCondition] = useState();
-  fetch(`https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}`, {mode: 'cors'})
-    .then((response) => {
-      return (response.json());
-    })
-    .then((response) => {
-      // console.log(response);
-      setCurrentTemp(response.current.temp_c);
-      setCondition(response.current.condition.text)
-      console.log(currentTemp);
-    })
+Navbar.propTypes = {
+  unit: PropTypes.string,
+  setUnit: PropTypes.func,
+  setSearchTerm: PropTypes.func,
+};
 
+function SearchBar({ setSearchTerm }) {
+  const [searchResults, setSearchResults] = useState([]);
+  const [time, setTime] = useState();
+  const [focused, setFocused] = useState(false);
+  var sched = "";
+
+  function handleChange(e) {
+    sched = e.target.value;
+    resetTimer();
+  }
+
+  function handleSearch() {
+    if (sched === "") {
+      setSearchResults([]);
+      return;
+    }
+    getSearchResults(sched).then((response) => {
+      setSearchResults(response);
+    });
+  }
+
+  async function getSearchResults(searchTerm) {
+    const response = await fetch(
+      `https://api.weatherapi.com/v1/search.json?key=${API_KEY}&q=${searchTerm}`,
+      { mode: "cors" }
+    );
+    return response.json();
+  }
+
+  function resetTimer() {
+    clearTimeout(time);
+    setTime(
+      setTimeout(() => {
+        handleSearch();
+      }, SEARCHTIMEOUTMS)
+    );
+  }
+
+  function commitSearchResult(result) {
+    if (result === undefined) return;
+    setSearchTerm(result.url);
+    document.getElementById("searchbar").value = "";
+    sched = "";
+    setSearchResults([]);
+  }
+
+  function SearchResults() {
+    if (searchResults.length === 0 || !focused) {
+      return;
+    }
+    return (
+      <div className="resultsbg">
+        {searchResults.map((r) => {
+          return (
+            <button
+              className="result"
+              key={r.id}
+              onClick={() => {
+                commitSearchResult(r);
+              }}
+            >
+              <h3>{r.name + ", " + r.region}</h3>
+            </button>
+          );
+        })}
+      </div>
+    );
+  }
+
+  function handleEnter(e) {
+    if (e.code === "Enter") {
+      setSearchTerm(e.target.value);
+    }
+  }
+  SearchResults.propTypes = {
+    searchResults: PropTypes.array,
+  };
+  const onFocus = () => {
+    setFocused(true);
+    console.log("focused");
+  };
+  const onBlur = (e) => {
+    if (e.relatedTarget !== null) return;
+    setFocused(false);
+    console.log("un focused");
+  };
+  return (
+    <>
+      <div className="searchBar" onBlur={onBlur} onFocus={onFocus}>
+        <input
+          id="searchbar"
+          type="text"
+          name=""
+          placeholder="Search"
+          onChange={handleChange}
+          onKeyDown={handleEnter}
+        />
+        <label hidden={true} htmlFor="searchbar">
+          Search
+        </label>
+        {searchResults.length !== 0 ? <SearchResults></SearchResults> : ""}
+      </div>
+    </>
+  );
+}
+
+SearchBar.propTypes = { setSearchTerm: PropTypes.func };
+
+function Hero({ searchTerm, unit }) {
+  const [currentTempCondition, setCurrentTempCondition] = useState({});
+
+  if (!("searchTerm" in currentTempCondition) || currentTempCondition.searchTerm !== searchTerm) {
+    fetch(
+      `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${searchTerm}&days=${FORECASTDAYS}`,
+      { mode: "cors" }
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((response) => {
+        console.log("hero fetch " + searchTerm);
+        setCurrentTempCondition({
+          searchTerm,
+          location: response.location.name,
+          temp_c: response.current.temp_c,
+          temp_f: response.current.temp_f,
+          condition: response.current.condition.text,
+          low_c: response.forecast.forecastday[0].day.mintemp_c,
+          low_f: response.forecast.forecastday[0].day.mintemp_f,
+          high_c: response.forecast.forecastday[0].day.maxtemp_c,
+          high_f: response.forecast.forecastday[0].day.maxtemp_f,
+        });
+      })
+      .catch((response) => {
+        console.log("Error: " + response);
+      });
+  }
   return (
     <>
       <div className="hero">
-        <h2 className="cityName">{city}</h2>
-        <h2 className="temperature">{currentTemp}°</h2>
-        <h2 className="weatherDesc">{condition}</h2>
+        <h1 className="cityName">{currentTempCondition.location}</h1>
+        <div>
+          <h2 className="temperature">
+            {Math.round(unit === "c" ? currentTempCondition.temp_c : currentTempCondition.temp_f)}
+            <span className="degree">°</span>
+          </h2>
+        </div>
+        <h2 className="weatherDesc">{currentTempCondition.condition}</h2>
         <div className="highLow">
-          <h2 className="highTemp">H: 25°</h2>
-          <h2 className="lowTemp">L: 16°</h2>
+          <h2 className="highTemp">
+            H:{Math.round(unit === "c" ? currentTempCondition.high_c : currentTempCondition.high_f)}
+            °
+          </h2>
+          <h2 className="lowTemp">
+            L:{Math.round(unit === "c" ? currentTempCondition.low_c : currentTempCondition.low_f)}°
+          </h2>
         </div>
       </div>
     </>
   );
 }
 
-function HourlyForecast() {
+Hero.propTypes = {
+  unit: PropTypes.string,
+  searchTerm: PropTypes.string,
+};
+
+function HourlyForecast({ searchTerm, unit }) {
+  const [hourModules, setModules] = useState([]);
+
+  if (fetched > fetchLimit) {
+    throw new Error("fetched too many times");
+  }
+  if (hourModules.length === 0 || hourModules[0].searchTerm !== searchTerm) {
+    fetch(
+      `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${searchTerm}&days=${FORECASTDAYS}`,
+      { mode: "cors" }
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((response) => {
+        let current = new Date(response.current.last_updated);
+        let currentHour = current.getHours();
+        console.log("Hourly forecast fetched");
+        fetched++;
+        let hourModTemp = [];
+
+        response.forecast.forecastday[0].hour
+          .slice(currentHour)
+          .concat(response.forecast.forecastday[1].hour.slice(0, currentHour + 1))
+          .forEach((e) => {
+            hourModTemp.push({
+              time: String(new Date(e.time).getHours()),
+              condition: e.condition.text,
+              conditionSrc: e.condition.icon,
+              temp_c: e.temp_c,
+              temp_f: e.temp_f,
+              searchTerm,
+            });
+          });
+        hourModTemp[0].time = "Now";
+        setModules(hourModTemp);
+      });
+  }
   return (
-    <div className="hourlyForecast">
-      <HourlyModule></HourlyModule>
-      <HourlyModule></HourlyModule>
-      <HourlyModule></HourlyModule>
-      <HourlyModule></HourlyModule>
-      <HourlyModule></HourlyModule>
-      <HourlyModule></HourlyModule>
-      <HourlyModule></HourlyModule>
-      <HourlyModule></HourlyModule>
-      <HourlyModule></HourlyModule>
-      <HourlyModule></HourlyModule>
-      <HourlyModule></HourlyModule>
-      <HourlyModule></HourlyModule>
+    <div className="hourlyForecastContainer">
+      <div className="tooltip">
+        <h4>Hourly Forecast</h4>
+      </div>
+      <div className="hourlyForecast">
+        {hourModules.map((mod) => (
+          <HourlyModule
+            unit={unit}
+            key={mod.time}
+            weather={{ ...mod, temp: unit === "c" ? mod.temp_c : mod.temp_f }}
+          ></HourlyModule>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-function DailyForecast() {
+HourlyForecast.propTypes = {
+  unit: PropTypes.string,
+  searchTerm: PropTypes.string,
+};
+
+function HourlyModule(props) {
+  return (
+    <div className="hourModule">
+      <h3>{props.weather.time}</h3>
+      <img src={"http:" + props.weather.conditionSrc} alt={props.weather.condition} />
+      <h3>{Math.round(props.weather.temp)}°</h3>
+    </div>
+  );
+}
+
+HourlyModule.propTypes = {
+  weather: PropTypes.shape({
+    time: PropTypes.string.isRequired,
+    condition: PropTypes.string,
+    conditionSrc: PropTypes.string,
+    temp_c: PropTypes.number,
+    temp_f: PropTypes.number,
+    temp: PropTypes.number,
+  }),
+  unit: PropTypes.string,
+};
+
+HourlyModule.defaultProps = {
+  weather: {
+    time: "Now",
+    condition: "☁",
+    conditionSrc: "",
+    temp: 25,
+  },
+};
+
+function DailyForecast({ searchTerm, unit }) {
+  const [dayModules, setDayModules] = useState([]);
+
+  if (fetched > fetchLimit) {
+    throw new Error("fetched too many times");
+  }
+  if (dayModules.length === 0 || dayModules[0].searchTerm !== searchTerm) {
+    fetch(
+      `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${searchTerm}&days=${FORECASTDAYS}`,
+      { mode: "cors" }
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((response) => {
+        // if (dayModules.length !== 0) return;
+        console.log("fetched in daily forecast ");
+        let dayModTemp = [];
+        let weekLow_c = 1000;
+        let weekHigh_c = -1000;
+        let weekLow_f = 1000;
+        let weekHigh_f = -1000;
+        response.forecast.forecastday.forEach((e) => {
+          weekLow_c = Math.min(weekLow_c, e.day.mintemp_c);
+          weekHigh_c = Math.max(weekHigh_c, e.day.maxtemp_c);
+
+          weekLow_f = Math.min(weekLow_f, e.day.mintemp_f);
+          weekHigh_f = Math.max(weekHigh_f, e.day.maxtemp_f);
+        });
+        response.forecast.forecastday.forEach((e) => {
+          dayModTemp.push({
+            searchTerm,
+            date: getDayName(e.date, "en-US"),
+            condition: e.day.condition.text,
+            conditionSrc: e.day.condition.icon,
+            low_c: e.day.mintemp_c,
+            high_c: e.day.maxtemp_c,
+            low_f: e.day.mintemp_f,
+            high_f: e.day.maxtemp_f,
+            weekLow_c,
+            weekLow_f,
+            weekHigh_c,
+            weekHigh_f,
+          });
+        });
+
+        dayModTemp[0].date = "Today";
+        dayModTemp[0].temp_c = response.current.temp_c;
+        dayModTemp[0].temp_f = response.current.temp_f;
+        setDayModules(dayModTemp);
+      });
+  }
   return (
     <div className="dailyForecast">
-      <DailyModule></DailyModule>
-      <DailyModule></DailyModule>
-      <DailyModule></DailyModule>
+      {dayModules.map((mod) => (
+        <DailyModule
+          key={mod.date}
+          weather={{
+            ...mod,
+            low: unit === "c" ? mod.low_c : mod.low_f,
+            high: unit === "c" ? mod.high_c : mod.high_f,
+            weekLow: unit === "c" ? mod.weekLow_c : mod.weekLow_f,
+            weekHigh: unit === "c" ? mod.weekHigh_c : mod.weekHigh_f,
+            temp: unit === "c" ? mod.temp_c : mod.temp_f,
+          }}
+        ></DailyModule>
+      ))}
     </div>
-  )
+  );
 }
 
-function HourlyModule() {
-  return (
-      <div className="hourModule">
-        <h3>Now</h3>
-        <h3>☁</h3>
-        <h3>24°</h3>
-      </div>
-  )
-}
+DailyForecast.propTypes = {
+  unit: PropTypes.string,
+  searchTerm: PropTypes.string,
+};
 
-function DailyModule() {
+function DailyModule(props) {
+  let weatherBarWidthPercent =
+    (100 * (props.weather.high_c - props.weather.low_c)) /
+    (props.weather.weekHigh_c - props.weather.weekLow_c);
+  let weatherBarLeftPercent =
+    (100 * (props.weather.low_c - props.weather.weekLow_c)) /
+    (props.weather.weekHigh_c - props.weather.weekLow_c);
+  let iconLeftPercent =
+    (100 * (props.weather.temp_c - props.weather.weekLow_c)) /
+    (props.weather.weekHigh_c - props.weather.weekLow_c);
+
   return (
     <div className="dayModule">
-      <h3>Today</h3>
-      <h3>☁</h3>
+      <h3>{props.weather.date}</h3>
+      <div className="imgContainer">
+        <img src={"http:" + props.weather.conditionSrc} alt={props.weather.condition} />
+      </div>
       <div className="weatherBar">
-        <h3>15°</h3>
+        <h3>{Math.round(props.weather.low)}°</h3>
         <div className="weatherBarBG">
-          <div className="weatherBarInner">
+          <div
+            className="weatherBarInner"
+            style={{
+              width: `${weatherBarWidthPercent}%`,
+              left: `${weatherBarLeftPercent}%`,
+            }}
+          >
+            {props.weather.date === "Today" ? (
+              <div className="curTempIconContainer" style={{ left: `${iconLeftPercent}%` }}>
+                <img src="/src/assets/react.svg" alt="" />
+              </div>
+            ) : (
+              ""
+            )}
           </div>
         </div>
-        <h3>28°</h3>
+        <h3>{Math.round(props.weather.high)}°</h3>
       </div>
     </div>
-  )
+  );
 }
 
+DailyModule.defaultProps = {
+  weather: {
+    date: "Today",
+    condition: "☁",
+    conditionSrc: "",
+    temp: 25,
+    low: 14,
+    high: 28,
+    weekLow: 14,
+    weekHigh: 30,
+  },
+};
+
+DailyModule.propTypes = {
+  weather: PropTypes.shape({
+    date: PropTypes.string.isRequired,
+    condition: PropTypes.string,
+    conditionSrc: PropTypes.string,
+    temp: PropTypes.number,
+    low: PropTypes.number,
+    high: PropTypes.number,
+    weekLow: PropTypes.number,
+    weekHigh: PropTypes.number,
+    temp_c: PropTypes.number,
+    low_c: PropTypes.number,
+    high_c: PropTypes.number,
+    weekLow_c: PropTypes.number,
+    weekHigh_c: PropTypes.number,
+  }),
+};
+
+function getDayName(dateStr, locale) {
+  var date = new Date(dateStr + "T00:00:00");
+  var str = date.toLocaleDateString(locale, { weekday: "short" });
+  return str;
+}
+
+function CFButton({ unit, setUnit }) {
+  let element = (
+    <div className="toggleCF">
+      <input onClick={onToggle} type="checkbox" id="switch" />
+      <label htmlFor="switch">Toggle Celsius and Fahrenheit</label>
+    </div>
+  );
+
+  function onToggle() {
+    setUnit(unit === "c" ? "f" : "c");
+  }
+  return element;
+}
+
+CFButton.propTypes = {
+  unit: PropTypes.string,
+  setUnit: PropTypes.func,
+  setSearchTerm: PropTypes.func,
+};
 export default App;
